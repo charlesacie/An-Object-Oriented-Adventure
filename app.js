ecaedd996fc4bcbf5f0ee13155f2a0a7eae5191a
charles young <charlesacieia@gmail.com>
2024-08-13 21:30:48 -0400

    new commit

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
+// Part one
 const adventurer = {
     name: "Robin",
     health: 10,
     inventory: ["sword", "potion", "artifact"],
     companion: {
-        name: "Leo",
-        type: "Cat",
-        companion: {
-            name: "Frank",
-            type: "Flea",
-            belongings: ["small hat", "sunglasses"]
-        }
+      name: "Ryah",
+      type: "Cat",
+      companion: {
+        name: "Frank",
+        type: "Flea",
+        inventory: ["small hat", "sunglasses"]
+      }
     },
     roll(mod = 0) {
-        const result = Math.floor(Math.random() * 20) + 1 + mod;
-        console.log(`${this.name} rolled a ${result}.`);
+      const result = Math.floor(Math.random() * 20) + 1 + mod;
+      console.log(`${this.name} rolled a ${result}.`);
     }
-};
+  };
+  for (let item of adventurer.inventory) {
+    console.log(item);
+  }
+  adventurer.roll();
